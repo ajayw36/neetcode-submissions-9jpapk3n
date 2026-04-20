25331a8e6e7e19cdea2df2aa5ac5c28e1c4110d7
Ajay Wadhwani <64669240+ajayw36@users.noreply.github.com>
2026-04-20 02:38:38 -0400

    Add: maximum-frequency-stack - submission-4

---
+++ Data Structures & Algorithms/maximum-frequency-stack/submission-4.py
@@ -0,0 +1,28 @@
+# Stack of Stacks
+from collections import defaultdict
+class FreqStack:
+    def __init__(self):
+        self.stacks = [[]]
+        self.counts = defaultdict(int)
+
+    def push(self, val: int) -> None:
+        self.counts[val] += 1
+        if self.counts[val] == len(self.stacks):
+            self.stacks.append([val])
+        else:
+            self.stacks[self.counts[val]].append(val)
+
+    def pop(self) -> int:
+        top_stack = self.stacks.pop()
+        top_element = top_stack.pop()
+        self.counts[top_element] -= 1
+        if top_stack:
+            self.stacks.append(top_stack)
+        return top_element
+
+
+
+# Your FreqStack object will be instantiated and called as such:
+# obj = FreqStack()
+# obj.push(val)
+# param_2 = obj.pop()
